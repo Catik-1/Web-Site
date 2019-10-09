030fe2203a31a87bc1cc37b9158e48698fad7c32
dragon26200387 <igor26200387@gmail.com>
2019-10-09 20:15:27 +0300

    hh
    hh

---
+++ WEB 2-0.py
@@ -0,0 +1,30 @@
+hosting_companies = [
+	{
+	"Company": "REG.ru",
+	"Тариф": "HOST-A: 95 rub, 1 Сайт, 7 GB SSD",
+	"Дней бесплатного периода": 14},
+	{
+	"Компания": "SpaceWeb",
+	"Тариф": "159 руб./мес., 5-20 сайтов, 5-15 ГБ SSD",
+	"Дней бесплатного периода": 14}
+	{
+	"Компания": "RU-CENTER",
+	"Тариф": "130 руб без Пайтона",
+	"Дней бесплатного периода": "-"
+	}
+	{
+	"Company": "MajorDomo",
+	"Cost": "99 rub/year",
+	"Day of Testing": 0},
+	{
+	"Company": "1GB",
+	"Cost": "0 rub/month",
+	"Day of Testing": 10},
+	{
+	"Company": "Beget.com",
+	"Cost": "140 rub/month",
+	"Day of Testing": 0},
+	{
+	"Company": "",
+	}
+]
